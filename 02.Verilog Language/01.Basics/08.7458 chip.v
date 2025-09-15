aa9377af0569fb97e5e13d7cb20caf2038cb6b2a
Adhi1727 <adhik8108@gmail.com>
2025-09-15 11:23:07 +0530

    Update 08.7458 chip.v

--- 02.Verilog Language/01.Basics/08.7458 chip.v
+++ 02.Verilog Language/01.Basics/08.7458 chip.v
@@ -1,1 +1,14 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    wire w,x,y,z;
+    assign w=(p1c&p1b&p1a);
+    assign x=(p1f&p1e&p1d);
+    assign p1y=w|x;
+    assign y=p2a&p2b;
+    assign z=p2d&p2c;
+    assign p2y=y|z;
 
+endmodule
