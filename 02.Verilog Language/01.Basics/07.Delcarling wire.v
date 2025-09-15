e87c890cb63193a3fb1c7aac536aae6290619794
Adhi1727 <adhik8108@gmail.com>
2025-09-15 11:14:34 +0530

    Update 07.Delcarling wire.v

--- 02.Verilog Language/01.Basics/07.Delcarling wire.v
+++ 02.Verilog Language/01.Basics/07.Delcarling wire.v
@@ -1,1 +1,15 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire x,z;
+    assign x=a&b;
+    assign z=c&d;
+    assign out=x|z;
+    assign out_n=~out;
 
+endmodule
