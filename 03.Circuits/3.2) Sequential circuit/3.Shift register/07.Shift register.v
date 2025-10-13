fd351a937b1139d387d0d0b8aadcf584b1d19db2
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-10-13 12:03:33 +0530

    Update 07.Shift register.v

--- 03.Circuits/3.2) Sequential circuit/3.Shift register/07.Shift register.v
+++ 03.Circuits/3.2) Sequential circuit/3.Shift register/07.Shift register.v
@@ -1,1 +1,19 @@
+module top_module (
+    input clk,
+    input resetn,   
+    input in,
+    output out);
+    reg [2:0] q;
+    
+    always @(posedge clk) begin
+        if(!resetn) begin
+            q <= 0;
+            out <= 0;
+        end
+        else begin
+            q <= {in,q[2:1]};
+            out <= q[0];
+        end
+    end
+endmodule
 
