7b4f6e3187eb01e564c1b8614a48de70b200e571
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-10-13 11:43:08 +0530

    Update 06.Lfsr_32bit.v

--- 03.Circuits/3.2) Sequential circuit/3.Shift register/06.Lfsr_32bit.v
+++ 03.Circuits/3.2) Sequential circuit/3.Shift register/06.Lfsr_32bit.v
@@ -1,1 +1,13 @@
+module top_module(
+    input clk,
+    input reset,    
+    output [31:0] q
+); 
+    always @(posedge clk) begin
+        if(reset)
+            q<=1;
+        else
+            q={1'b0^q[0],q[31:23],q[22]^q[0],q[21:3],q[2]^q[0],q[1]^q[0]};
+    end
 
+endmodule
