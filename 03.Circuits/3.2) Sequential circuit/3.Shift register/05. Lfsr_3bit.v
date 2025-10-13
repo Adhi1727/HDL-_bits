4325ded57e7d4666b58b1babf32375c3cc8088a6
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-10-13 10:45:26 +0530

    Update 05. Lfsr_3bit.v

--- 03.Circuits/3.2) Sequential circuit/3.Shift register/05. Lfsr_3bit.v
+++ 03.Circuits/3.2) Sequential circuit/3.Shift register/05. Lfsr_3bit.v
@@ -1,1 +1,19 @@
+module top_module (
+	input [2:0] SW,      
+    input [1:0] KEY,     
+	output [2:0] LEDR);  
+    
+    wire L = KEY[1];
+    wire clk = KEY[0];
+    reg [2:0] Q;
+    
+    always @(posedge clk) begin
+        if(L)
+            Q<=SW;
+        else
+            Q <= {Q[1]^Q[2],Q[0],Q[2]};
+    end
+    
+     assign LEDR = Q;
+ endmodule
 
