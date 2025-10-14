6deb306671abd1737c7d908b02f1177800f7348f
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-10-14 09:40:49 +0530

    Create 03. Async_simple_fsm2.v

---
+++ 03.Circuits/3.2) Sequential circuit/5. Finite State machine/03. Async_simple_fsm2.v
@@ -0,0 +1,32 @@
+module top_module(
+    input clk,
+    input areset,    
+    input j,
+    input k,
+    output out);   
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+            OFF:if(j==1)
+                next_state=ON;
+            else
+                next_state=OFF;
+            ON:if(k==1)
+                next_state=OFF;
+            else 
+                next_state=ON;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if(areset)
+            state=OFF;
+        else
+            state=next_state;
+    end
+            assign out = (state==ON);
+           
+            endmodule
